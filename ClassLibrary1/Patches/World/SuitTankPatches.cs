75756e18346ff6a19275c9dc6f6f217cf0f3e09b
Lyraedan <61066562+Lyraedan@users.noreply.github.com>
2026-05-03 19:04:16 +0100

    Suit tank GetTankAmount fix

---
+++ ClassLibrary1/Patches/World/SuitTankPatches.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using HarmonyLib;
+
+namespace ONI_MP.Patches.World
+{
+    [HarmonyPatch(typeof(SuitTank), nameof(SuitTank.GetTankAmount))]
+    public static class SuitTank_GetTankAmount_Patch
+    {
+        // For the sake of the dupes lives (and notifications on clients) default to full
+        private const float DEFAULT_FULL_TANK = 75f;
+
+        public static bool Prefix(SuitTank __instance, ref float __result)
+        {
+            // If the object itself is invalid, just return 0
+            if (__instance == null)
+            {
+                __result = DEFAULT_FULL_TANK;
+                return false;
+            }
+
+            if (__instance.storage == null)
+            {
+                __instance.storage = __instance.GetComponent<Storage>();
+
+                if (__instance.storage == null)
+                {
+                    __result = DEFAULT_FULL_TANK;
+                    return false;
+                }
+            }
+
+            __result = __instance.storage.GetMassAvailable(__instance.elementTag);
+            return false; // skip original
+        }
+    }
+}

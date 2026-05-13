319b7eb33a628143f22b65c606d76bc7351b7fcd
Lyraedan <61066562+Lyraedan@users.noreply.github.com>
2026-05-13 19:14:14 +0100

    Removed unused imports

--- ClassLibrary1/Networking/Components/StructureStateSyncer.cs
+++ ClassLibrary1/Networking/Components/StructureStateSyncer.cs
@@ -1,18 +1,14 @@
 using System;
 using System.Collections.Generic;
-using KSerialization;
 using ONI_MP.DebugTools;
 using ONI_MP.Misc;
 using ONI_MP.Networking.Packets.World;
-using ONI_MP.Patches.GamePatches;
 using ONI_MP.Patches.World;
-using Rendering;
 using Shared.Profiling;
 using UnityEngine;
+
 using static EnergyGenerator;
 using static ONI_MP.Networking.Packets.World.StructureStatePacket;
-using static STRINGS.UI.ELEMENTAL;
-using static STRINGS.UI.OVERLAYS;
 
 namespace ONI_MP.Networking.Components
 {
